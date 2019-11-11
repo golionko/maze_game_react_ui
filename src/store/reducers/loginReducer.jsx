de224d277419e3990de757dd1c7f67537951f671
golionko <golionko@gmail.com>
2019-11-11 17:54:21 -0500

    ISSUE-3-User_Auth

--- src/store/reducers/loginReducer.jsx
+++ src/store/reducers/loginReducer.jsx
@@ -1,22 +1,47 @@
 import ActionTypes from '../consts/ActionTypes';
 
 const defaultState = {
-    logged : false
+    isLoading: false,
+    logged: false,
+    errMsg: '',
+    user: {}
   };
 
 const loginReducer = (state = defaultState, action) => {
     switch (action.type) {
-        case ActionTypes.LOGIN: {
+        case ActionTypes.LOGIN_LAODING: {
             return {
                 ...state,
-                logged: true
+                isLoading: true,
+                errMsg: '',
+                logged: false,
+                user : {}
               };
         }
-
+        case ActionTypes.LOGIN_SUCCESS: {
+            return {
+                ...state,
+                logged: true,
+                isLoading: false,
+                user: action.user
+              };
+        }
+        case ActionTypes.LOGIN_FAILURE: {
+            return {
+                ...state,
+                isLoading: false,
+                logged: false,
+                errMsg: action.errMsg,
+                user : {}
+              };
+        }
         case ActionTypes.LOGOUT: {
             return {
                 ...state,
-                logged: false
+                isLoading: false,
+                logged: false,
+                errMsg: '',
+                user: {}
               };
         }
 
