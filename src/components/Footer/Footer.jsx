b3835b8a2bfa925161bfcddab239d6c0063b0533
golionko <golionko@gmail.com>
2019-11-16 14:19:51 -0500

    home page logged out

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-
 const Footer = () => (
-        <div className="pl-5 pt-1 pb-1 mt-3 bg-dark text-white h6">© 2019 AGC All Rights Reserved</div>
-    );
+  <div className="pl-5 pt-1 pb-1 mt-3 bg-dark text-white h6">
+    © 2019 AGC All Rights Reserved
+  </div>
+);
 
 export default Footer;
