de1951d99305151202aac7957ac77fc62e04c05c
golionko <golionko@gmail.com>
2019-11-24 14:06:33 -0500

    fix header css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,17 +5,17 @@ import { Link } from "react-router-dom";
 import { formatDateTime } from "../../util/helpers";
 
 const Header = () => (
-  <div>
-    <Navbar bg="dark" variant="dark" className="pb-3">
-      <Navbar.Brand className="ml-5" as={Link} to="/">
+  <div className="h-100 bg-dark">
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand className="ml-5  mt-2" as={Link} to="/">
         <img
           alt=""
           src="/logo.png"
-          width="30"
-          height="30"
+          width="40"
+          height="40"
           className="d-inline-block align-top"
         />
-        {" Maze"}
+        {" Maze"} 
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
