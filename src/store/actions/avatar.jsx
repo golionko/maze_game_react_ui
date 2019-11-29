75818b77ebb262f57904efcfd1b00bdc4460c77a
golionko <golionko@gmail.com>
2019-11-28 22:11:08 -0500

    Connect getAvatar to backend

--- src/store/actions/avatar.jsx
+++ src/store/actions/avatar.jsx
@@ -1,4 +1,5 @@
 import { AvatarActionTypes } from "../consts/ActionTypes";
+import { remote } from "../../util/remote";
 
 export const avatarDataLoading = () => ({
   type: AvatarActionTypes.AVATAR_DATA_LAODING
@@ -13,23 +14,14 @@ export const avatarDataFailure = () => ({
   type: AvatarActionTypes.AVATAR_DATA_FAILURE
 });
 
-export const getAvatarData = userId => dispatch => {
+export const getAvatarData = id => dispatch => {
   dispatch(avatarDataLoading());
-  dispatch(
-    avatarDataSuccess({
-      id: 1,
-      name: "Mr Reaper",
-      level: 1,
-      strength: 3,
-      dexterity: 5,
-      luck: 7,
-      constitution: 5,
-      xp: 25,
-      maxXp: 135,
-      energy: 23,
-      maxEnergy: 100,
-      hp: 67,
-      maxHp: 98
-    })
-  );
+
+  remote().get('/avatar/' + id)
+  .then(function (response) {
+    dispatch(avatarDataSuccess(response.data));
+  })
+  .catch(function (error) {
+    dispatch(avatarDataFailure());
+  });
 };
